9237802b195495b0c742af5e3ff6cf6f95521500
Nathan Glover <nathan@glovers.id.au>
2020-04-16 09:59:59 +0800

    updated api key

--- app/src/aws-exports.js
+++ app/src/aws-exports.js
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://bj6l3bgujzcbfn5srepdfonkhu.appsync-api.ap-southeast-2.amazonaws.com/graphql",
     "aws_appsync_region": "ap-southeast-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-rfqlxbm5kje2diylxgcjq2b6bm"
+    "aws_appsync_apiKey": "da2-xm2l3qhwb5bglh37pobyruvcbi"
 };
 
 
